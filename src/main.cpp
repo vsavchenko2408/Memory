1963e77a440568049b9c4dbc64649ffc0f627ffe
Der Meister <v.savchenko2408@gmail.com>
2026-03-25 04:30:39 +0100

    clean test in main

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,5 @@ int main()
 
     Shared_ptr<int> sptr(32);
     Weak_ptr<int> wptr(sptr);
- 
-std::cout << wptr.expired() << std::endl; 
 
-
-auto lck = wptr.lock();
-if(lck.get() == nullptr)
-    std::cout << "Object destroyed" << std::endl;
-
-
-struct Node {
-    int value;
-    Shared_ptr<Node> next;
-    Weak_ptr<Node> prev; 
-};
 }
